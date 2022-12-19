9c6ec771962088b2a53980d0114d2bcd7839310b
THSlimes <thomkastelein.tk@gmail.com>
2022-12-19 15:04:33 +0100

    got camera output

---
+++ public/scripts/get-camera.js
@@ -0,0 +1,12 @@
+const video = document.getElementById('camera');
+
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(function(stream) {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
